b561adbd72c473377e5796f697f170c35a8436a6
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-06-05 23:22:28 +0300

    Routes updates

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,9 @@ import './App.css';
 import { getMovieByQuery, getMovieCredits, getMovieDetails, getMovieReviews, getTrendingMovies } from '../tmdb-api-fetch';
 import Navigation from '../Navigation/Navigation';
 import { Route, Routes } from 'react-router-dom';
+import HomePage from '../../pages/HomePage/HomePage';
+import MoviesPage from '../../pages/MoviesPage/MoviesPage';
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
 
 function App() {
   useEffect(() => {
